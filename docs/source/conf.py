f3cc98e523bf247a1d885ecbe4fea7aefef6daed
Peyton <peynmurray@gmail.com>
2018-05-16 16:49:46 -0700

    Try to get around readthedocs build issues with PyQt5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.napoleon'
 ]
 
+autodoc_mock_imports = ['PyQt5', 'PyQt5.QtGui', 'PyQt5.QtCore', 'PyQt5.QtWidgets']
 napoleon_use_ivar = True
 
 mathjax_path = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js"
