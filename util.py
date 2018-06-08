d2034779b4f84f7a334e75a169210021648fb526
Peyton <peynmurray@gmail.com>
2018-06-07 19:07:58 -0700

    Added pure python implementations of np.where and np.argwhere, somehow numba doesn't like numpy's versions

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import numpy as np
 import numba as nb
 
+import hypothesis
+
 
 @nb.jit(nb.float64[:, :](nb.float64[:, :], nb.float64[:, :]), nopython=True, nogil=True)
 def fast_symmetric_convolve(input, kernel):
@@ -20,3 +22,42 @@ def fast_symmetric_convolve(input, kernel):
 @nb.jit(nopython=True, nogil=True)
 def line(x, a, b):
     return a*x+b
+
+
+# @nb.jit(nb.int64(nb.float64[:]), nopython=True, nogil=True)
+# @nb.jit(nopython=True, nogil=True)
+def arg_first_not_nan(arr):
+    i = np.argwhere(np.logical_not(np.isnan(arr)))
+    print(type(i))
+    if i.shape[0] == 0:
+        raise ValueError("Array is only filled with nan values")
+    else:
+        return i[0][0]
+
+
+# @nb.jit(nb.int64(nb.float64[:]), nopython=True, nogil=True)
+# @nb.jit(nopython=True, nogil=True)
+def arg_last_non_nan(arr):
+    return arr.shape[0] - 1 - arg_first_not_nan(np.flip(arr, 0))
+
+
+# nb_argwhere and nb_where are potentially faster than numpy's equivalent functions, but have not been validated.
+# Use with caution.
+@nb.jit(nopython=True, nogil=True)
+def nb_argwhere(arr):
+    is_nonzero = nb_where(arr)
+    ret = np.empty((1, np.sum(is_nonzero)))
+    j = 0
+    for i in range(arr.shape[0]):
+        if is_nonzero[i]:
+            ret[j] = i
+            j += 1
+    return ret
+
+
+@nb.jit(nopython=True, nogil=True)
+def nb_where(arr):
+    ret = np.empty_like(arr)
+    for i in range(arr.shape[0]):
+        ret[i] = True if arr[i] != 0 else False
+    return ret
